95d6a641c93df833afc8b716c7903bbd08174fce
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-09-18 17:05:53 -0300

    Add files via upload

--- Calvo/Entrada/urls.py
+++ Calvo/Entrada/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('cadastrar_user/', views.cadastrar_user, name="cadastrar_user")
+    path('cadastrar_user/', views.cadastrar_user, name="cadastrar_user"),
+    path('mandar_msg/', views.envia_msg, name="mandar_msg")
 ]
--- Calvo/Entrada/views.py
+++ Calvo/Entrada/views.py
@@ -1,10 +1,11 @@
 import email
 from django.shortcuts import render
 from .models import User
+from .models import Mensage
 
 # Create your views here.
 def home(request):
-    return render(request,'teste.html')
+    return render(request,'mandar_msg.html')
 
 def cadastrar_user(request):
     nomeForm = request.POST.get("user")
@@ -17,4 +18,21 @@ def cadastrar_user(request):
 
     usuario.save()
 
-    return render(request, 'listUser.html', {"usuario": usuario})
+    usuarios = User.objects.all()
+    users = {"user": usuarios}
+
+    return render(request, 'listUser.html', users)
+
+def envia_msg(request):
+    mensagemForm = request.POST.get("msg")
+    idUser = request.POST.get("ID")
+
+    mensagem = Mensage(mensage=mensagemForm,
+                        id_user=idUser)
+
+    mensagem.save()
+
+    mensagens = Mensage.objects.all()
+    mensages = {"msg": mensagens}
+
+    return render(request, 'listUser.html', mensages)
