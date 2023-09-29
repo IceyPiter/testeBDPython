c19587a03ed3d2bcb15a4158c1e4847e7de2ee10
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2023-09-28 22:41:16 -0300

    Update models.py

--- Calvo/Entrada/models.py
+++ Calvo/Entrada/models.py
@@ -15,6 +15,7 @@ class Mensage(models.Model):
     id = models.AutoField(primary_key=True)
     mensage = models.CharField(max_length=50, blank=True)
     id_user = models.CharField(max_length=3, blank=True)
+    key = models.ForeignKey("self", related_name="self", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.mensage
